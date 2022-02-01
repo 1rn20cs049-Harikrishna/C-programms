7946fc242b94f4a1f5e97b550e85f40463533bcf
Harikrishna R <95685588+1rn20cs049-Harikrishna@users.noreply.github.com>
2022-02-01 08:06:09 +0530

    Update Array_reversal.c

--- Array_reversal.c
+++ Array_reversal.c
@@ -4,8 +4,10 @@
 int main()
 {
     int num, *arr,*arr2,i;
+    printf("enter the aize of an array\n");
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
+    printf("Enter the array elements\n");
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
@@ -13,6 +15,7 @@ int main()
     for(i=1;i<=num;i++) {
         arr2[i-1] = arr[num-i];
     }
+    printf("Array elements in Reverse order is \n");
     for(i = 0; i < num; i++)
         printf("%d ", *(arr2 + i));
     return 0;
